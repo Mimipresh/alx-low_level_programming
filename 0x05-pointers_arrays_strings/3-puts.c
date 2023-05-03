b53f45fa8cc18c0f56d5df937fe34a0bf28f6b63
mimipresh <mimipresh122@gmail.com>
2023-05-02 17:10:17 -0700

    task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - print a string, followed by a new line, to stdout
+ * _puts - print a string, followed by a new line, to stdout 
  * @str: string to print
  */
 void _puts(char *str)
